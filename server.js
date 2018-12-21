eb1c2b72372ba8c23e6972e72f8076613b62ef6d
Ryan Brennan <ryanbrennan@ryans-mbp.gateway.sonic.net>
2018-12-21 14:23:13 -0800

    added morgan to production renamed start script w/o nodemon

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
 const app = express();
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.use('/', express.static(__dirname + '/public/'));
-
-app.listen(PORT, () => {
-  console.log(`...listening on port ${PORT}`);
+app.get("/*", function (req, res){
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-
+app.listen(port, () => {
+  console.log(`server running port: ${port}`);
+});
